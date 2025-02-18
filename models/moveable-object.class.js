7ea6b5f2450b64da2c7255429d5f6a132082dddd
mian Shargeel <call2poma@gmail.com>
2025-02-18 08:25:10 +0100

    loadPepeImages-function created

--- models/moveable-object.class.js
+++ models/moveable-object.class.js
@@ -4,10 +4,18 @@ class MoveableObject {
   img;
   height = 150; //objects height[pepe, chicken..]
   width = 100;
+  imageCache = []; //will store the character-img[pepe]
   
   loadImage(path) {
     this.img = new Image();//creating img-tag through JS-Image-Object
     this.img.src = path;
+  }
+  loadPepeImages(arrImg) { //to load all Pepe's images
+    arrImg.forEach(path => {
+      let img = new Image();
+      img.src = path; 
+      this.loadPepeImages.push(img);
+    });
   }
   moveRight() {
     console.log('Moving right');
